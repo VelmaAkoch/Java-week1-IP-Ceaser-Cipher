88e08640843400b63460274c42568ef41345b1df
VelmaAkoch <velmaakochi1@gmail.com>
2022-04-27 20:10:53 +0300

    add all changes done on App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
-public class App {
+ public class App {
     public static void main(String[] args){
-        Scanner in = new Scanner (System.in);
+        Scanner in = new Scanner(System.in);
         System.out.println("1: Encryption\n2: Decryption");
         int menu = in.nextInt();
 
         if (menu == 1){
-            System. out. println("Please enter a message to be encrypted:" );
+            System. out. println("Please enter a message to be encrypted: ");
             String input = in.next();
-            System.out.println("Please enter a number to be your shift key:");
+            System.out.println("Please enter a number to be your shift key: ");
             int keyInput = in.nextInt();
             String output = "";
 
@@ -25,24 +25,24 @@ public class App {
         }
 
         else if (menu == 2){
-            System. out. println("Please enter a message to be decrypted;");
-            String input = in.next();
-            System. out. println("Please enter a number to be your decryption key");
-            int keyInput = in.nextInt();
-            String output = "";
-
-            for(int i = 0; i < input.length(); i++){
-                DecryptCipher encoding = new DecryptCipher();
-                encoding.setKey(keyInput);
-                encoding.setC(input.charAt(i));
-                output += encoding.decrypt();
+                System. out. println("Please enter a message to be decrypted: ");
+                String input = in.next();
+                System. out. println("Please enter a number to be your decryption key: ");
+                int keyInput = in.nextInt();
+                String output = "";
+                for(int i = 0; i < input.length(); i++){
+                    DecryptCipher encoding = new DecryptCipher();
+                    encoding.setKey(keyInput);
+                    encoding.setC(input.charAt(i));
+                    output += encoding.decrypt();
+                }
                 System.out.println("Your decrypted message is: "+output);
-            }
 
-//            else
-//            {
-//                System.out.println("You should select 1 or 2");
-//            }
+        }
+
+        else
+        {
+            System.out.println("You should select 1 or 2");
         }
     }
 }
